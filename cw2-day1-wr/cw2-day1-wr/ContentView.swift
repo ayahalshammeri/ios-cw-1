d597b3ae17f9379aafafa644fd638469ec3af98b
AyahAlshammeri <66526830+jumana-alshameri@users.noreply.github.com>
2022-03-02 19:54:03 +0300

    Doneeeeee

---
+++ cw2-day1-wr/cw2-day1-wr/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw2-day1-wr
+//
+//  Created by jumana alshameri on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw2-day1-wr/cw2-day1-wr/cw2_day1_wrApp.swift
@@ -0,0 +1,17 @@
+//
+//  cw2_day1_wrApp.swift
+//  cw2-day1-wr
+//
+//  Created by jumana alshameri on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw2_day1_wrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
